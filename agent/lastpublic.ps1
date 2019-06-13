919d07f1e8bc0a0239180aa556f425ca8208da25
Ivan Bencaric <ivan.bencaric@carnet.hr>
2019-06-13 11:14:51 +0200

    Initial with ipify and RIPE Database RESTful API

--- agent/lastpublic.ps1
+++ agent/lastpublic.ps1
@@ -1,15 +1,33 @@
 # Plugin "Get IP" OCSInventory
-# Author: Valentin COSSE & Valentin DEVILLE
+# Author CARNET
+# Original : Valentin COSSE & Valentin DEVILLE
 
+$ripeOrg     = [String]::Empty;
+$ripeNetName = [String]::Empty;
 
-$myjson = Invoke-RestMethod -Uri https://www.ipinfo.io/json
+$ipify = Invoke-RestMethod -Uri "https://api.ipify.org?format=text";
+$ripeWhois = (Invoke-RestMethod -Uri "https://stat.ripe.net/data/whois/data.json?resource=$ipify").data.records;
+
+for ($ripeKey=0; $ripeKey -le $ripeWhois.value.Count; $ripeKey++) {
+    if ($ripeWhois.key[$ripeKey] -eq "descr") {
+        $ripeOrg += $ripeWhois.value[$ripeKey] +", ";
+    }
+    elseif ($ripeWhois.key[$ripeKey] -eq "netname") {
+        $ripeNetName += $ripeWhois.value[$ripeKey] +", ";
+    }
+}
+
+$ripeOrg     = $ripeOrg.TrimEnd();
+$ripeNetName = $ripeNetName.TrimEnd();
+
+if ($ripeOrg.Substring($ripeOrg.Length-1,1) -eq ",") { $ripeOrg = $ripeOrg.Substring(0,$ripeOrg.Length-1) }
+if ($ripeNetName.Substring($ripeNetName.Length-1,1) -eq ",") { $ripeNetName = $ripeNetName.Substring(0,$ripeNetName.Length-1) }
 
 $xml = '<LASTPUBLICIP>'
-$xml += '<IP>' + $myjson.ip + '</IP>'
-$xml += '<CITY>' + $myjson.city + '</CITY>'
-$xml += '<ORG>' + $myjson.org + '</ORG>'
+$xml += '<IP>' + $ipify + '</IP>'
+$xml += '<CITY>' + $ripeNetName + '</CITY>'
+$xml += '<ORG>' + $ripeOrg + '</ORG>'
 $xml += '</LASTPUBLICIP>'
-
 
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 [Console]::WriteLine($xml)
