b3cb09f8a468aff71ccf043480b8140363567f79
Ivan Bencaric <ivan.bencaric@carnet.hr>
2019-10-01 14:37:21 +0200

    Some fixes.

--- agent/lastpublic.ps1
+++ agent/lastpublic.ps1
@@ -5,26 +5,34 @@
 $ripeOrg     = [String]::Empty;
 $ripeNetName = [String]::Empty;
 
+$errorMessage = "NO INTERNET CONNECTION";
+
 $getIPUri = "http://bwm4.carnet.hr:8080/live/ip.php";
-# $getIPUri = "https://api.ipify.org?format=text";
 
-$ip = Invoke-RestMethod -Uri $getIPUri;
-$ripeWhois = (Invoke-RestMethod -Uri "https://stat.ripe.net/data/whois/data.json?resource=$ip").data.records;
-
-for ($ripeKey=0; $ripeKey -le $ripeWhois.value.Count; $ripeKey++) {
-    if ($ripeWhois.key[$ripeKey] -eq "descr") {
-        $ripeOrg += $ripeWhois.value[$ripeKey] +", ";
+if (Test-NetConnection) {
+    $ip = Invoke-RestMethod -Uri $getIPUri;
+    $ripeWhois = (Invoke-RestMethod -Uri "https://stat.ripe.net/data/whois/data.json?resource=$ip").data.records;
+    
+    for ($ripeKey=0; $ripeKey -le $ripeWhois.value.Count; $ripeKey++) {
+        if ($ripeWhois.key[$ripeKey] -eq "descr") {
+            $ripeOrg += $ripeWhois.value[$ripeKey] +", ";
+        }
+        elseif ($ripeWhois.key[$ripeKey] -eq "netname") {
+            $ripeNetName += $ripeWhois.value[$ripeKey] +", ";
+        }
     }
-    elseif ($ripeWhois.key[$ripeKey] -eq "netname") {
-        $ripeNetName += $ripeWhois.value[$ripeKey] +", ";
-    }
+    
+    $ripeOrg     = $ripeOrg.TrimEnd();
+    $ripeNetName = $ripeNetName.TrimEnd();
+    
+    if ($ripeOrg.Length -gt 0) { if ($ripeOrg.Substring($ripeOrg.Length-1,1) -eq ",") { $ripeOrg = $ripeOrg.Substring(0,$ripeOrg.Length-1) } }
+    if ($ripeNetName -gt 0) { if ($ripeNetName.Substring($ripeNetName.Length-1,1) -eq ",") { $ripeNetName = $ripeNetName.Substring(0,$ripeNetName.Length-1) } }
 }
-
-$ripeOrg     = $ripeOrg.TrimEnd();
-$ripeNetName = $ripeNetName.TrimEnd();
-
-if ($ripeOrg.Length -gt 0) { if ($ripeOrg.Substring($ripeOrg.Length-1,1) -eq ",") { $ripeOrg = $ripeOrg.Substring(0,$ripeOrg.Length-1) } }
-if ($ripeNetName -gt 0) { if ($ripeNetName.Substring($ripeNetName.Length-1,1) -eq ",") { $ripeNetName = $ripeNetName.Substring(0,$ripeNetName.Length-1) } }
+else {
+    $ip = $errorMessage;
+    $ripeOrg = $errorMessage;
+    $ripeNetName = $errorMessage;
+}
 
 $xml = '<LASTPUBLICIP>'
 $xml += '<IP>' + $ip + '</IP>'
